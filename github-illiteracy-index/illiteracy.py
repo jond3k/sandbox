e63bdd8802d98ab59589fb46bf6571662382a74e
Jon <jond3k@gmail.com>
2012-01-16 13:49:44 +0000

    Can now search for all languages (please don't do this often :-))

--- github-illiteracy-index/illiteracy.py
+++ github-illiteracy-index/illiteracy.py
@@ -7,13 +7,22 @@ LANGS_URL   = 'https://github.com/languages'
 SEARCH_URL  = 'https://github.com/search?type=Code&language=%s&q=%s'
 
 SEARCH_REGEX = r'<div class="title">Code \(([0-9]+)\)</div>'
-LANGS_REGEX  = r'<a href="/languages/([^\b"]*)"\s*class="bar"\s*style="width: [0-9]*%">\s*([0-9]*)%</a>'
+# match the top 10 languages
+LANGS_REGEX  = r'<a href="/languages/([^\b"]*)"\s*class="bar"\s*style="width: [0-9]*%">\s*[0-9]*%</a>'
+# match all languages
+#LANGS_REGEX  = r'<li>\s*<a href="/languages/([^\b"]*)"\s*>[^\b"]*</a>\s*</li>'
 
 TERMS = {'length': 'lenght', 'height': 'heigth', 'hierarchy':'heirarchy'}
 
-def request(url):
+def request(url, retries=2):
     print "Requesting: %s" % url
-    return urllib2.urlopen(url).read()
+    try:
+        return urllib2.urlopen(url).read()
+    except HTTPError as e:
+        if retries == 0:
+            raise e
+        print e
+        return request(url, retries-1)
 
 def extractSearchCount(body):
     result = 0
@@ -28,35 +37,12 @@ def getSearchCount(lang, term):
     return result
 
 def getLangs():
-    results = {}
+    results = []
     body    = request(LANGS_URL)
     matches = re.findall(LANGS_REGEX, body)
     for match in matches:
-        results[match[0]] = float(match[1])
+        results.append(match)
     return results
-
-def topLanguage(langs):
-    return max(langs, key=lambda a: langs.get(a))
-
-def normalizeLangs(langs):
-    top        = topLanguage(langs)
-    max        = langs[top]
-    normalized = {}
-
-    print "Top language is %s (%s%%)" % (top, max)
-    
-    for lang in langs:
-        freq = langs[lang]
-        if freq > 0:
-            normalized[lang] = max / freq
-
-    return normalized
-
-def printIndex(normalized):
-    format = '%-12s%-12s%s'
-    print format % ("Language", "Percentage", "Normalized")
-    for lang in normalized:
-        print format % (lang, langs[lang], round(normalized[lang],2))
 
 def getLangFreqs(lang, terms):
     results = {}
@@ -64,7 +50,9 @@ def getLangFreqs(lang, terms):
         failWord = terms[goodWord]
         fails  = getSearchCount(lang, failWord)
         passes = getSearchCount(lang, goodWord)
-        freq   = fails/passes
+        freq = 0
+        if (passes > 0):
+            freq   = fails/passes
         print "%s: %s = %s, %s = %s, Rate = %s" % (lang, goodWord, passes, failWord, fails, freq)
         results[goodWord] = freq
     return results
@@ -98,9 +86,8 @@ def printSorted(sorted):
         i += 1
         print format % (i,  urllib2.unquote(result[0]), round(result[1], 8))
 
-#langs = {'JavaScript': 20.0}
-langs      = getLangs()
-
+#langs = ['JavaScript']
+langs    = getLangs()
 freqs    = getAllFreqs(langs, TERMS)
 averaged = averageFreqs(freqs)
 sorted   = sortResults(averaged)
